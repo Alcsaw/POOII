2eb51c2d463f67080ebd41dacbdaff291ace0249
Augusto Luiz da Costa Schnorr <m98567@ACAD.UNISC.BR>
2019-10-17 22:18:52 -0300

    adds aula11 - begin2

--- Aula11/src/Views/TotalizacaoVotos.java
+++ Aula11/src/Views/TotalizacaoVotos.java
@@ -35,20 +35,20 @@ public class TotalizacaoVotos extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null},
-                {null, null},
-                {null, null},
-                {null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Nome Candidato", "Quantidade de Votos"
+                "Nome Candidato", "Legenda", "Quantidade de Votos"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.Integer.class
+                java.lang.String.class, java.lang.String.class, java.lang.Integer.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false
+                false, true, false
             };
 
             public Class getColumnClass(int columnIndex) {
--- Aula11/src/models/Votos.java
+++ Aula11/src/models/Votos.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
  * @author m98567
  */
 public class Votos {
+
+   /* public void leitura() throws IOException {
+        BufferedReader buff;
+        try {
+            FileReader in = new FileReader("arq.txt");
+            buff = new BufferedReader(in);
+            try {
+                String line;
+                do {
+                    line = buff.readLine();
+                    System.out.println(line);
+                } while (line != null);
+            } catch (IOException iOException) {
+                System.out.println(iOException);
+            }
+            finally {
+                buff.close();
+            }
+        } catch (FileNotFoundException fileNotFound) {
+            System.out.println(fileNotFound);
+        }
+    }*/
     
+    public ArrayList<
 }
+
+    
